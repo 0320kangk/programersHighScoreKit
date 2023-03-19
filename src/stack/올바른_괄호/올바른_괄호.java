cf2e31ac1d75e98bfae10a08ba27269c986e1613
0320kangk <0320kangk@naver.com>
2023-03-20 07:16:29 +0900

    올바른 괄호 풀이

---
+++ src/stack/올바른_괄호/올바른_괄호.java
@@ -0,0 +1,31 @@
+package stack.올바른_괄호;
+
+import java.util.Stack;
+
+public class 올바른_괄호 {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        // {는 stack에 넣기, }는 스택에서 빼기, 만약 스택이 비어 있다면 오류
+        for (int i = 0; i < s.length(); i++) {
+            if ( s.charAt(i) == '(') {
+                stack.add('(');
+            } else if (!stack.empty()) {
+               stack.pop();
+            } else {
+                return false;
+            }
+        }
+        if (!stack.empty())
+            answer = false;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        올바른_괄호 올바른_괄호 = new 올바른_괄호();
+        boolean solution = 올바른_괄호.solution("(())");
+        System.out.println(solution);
+
+    }
+
+}
