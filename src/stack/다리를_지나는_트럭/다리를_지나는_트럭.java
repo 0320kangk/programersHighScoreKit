a25e665619225eefe12f4718778477a86f585906
0320kangk <0320kangk@naver.com>
2023-03-23 14:15:10 +0900

    다리를 지나는 트럭 풀이

--- src/stack/다리를_지나는_트럭/다리를_지나는_트럭.java
+++ src/stack/다리를_지나는_트럭/다리를_지나는_트럭.java
@@ -1,4 +1,4 @@
-package stack;
+package stack.다리를_지나는_트럭;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -6,15 +6,36 @@ import java.util.Queue;
 public class 다리를_지나는_트럭 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-
-        Queue<truck> weightsQueue = new LinkedList<>();
+        int answer = 0;
+        int totalWeight = 0;
+        int index = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        //무게와 길이를 생각해야 한다
+        for (int i = 0; i < bridge_length; i++) {
+            queue.add(0);
+        }
+        while( index < truck_weights.length) {
+            totalWeight -= queue.poll();
+            //넣는 기준은 트럭의 무게와 길이
+            if (queue.size() < bridge_length &&
+                    weight >= totalWeight + truck_weights[index]) {
+                totalWeight += truck_weights[index];
+                queue.add(truck_weights[index++]);
+            }
+            else {
+                queue.add(0);
+            }
+            answer++;
+        }
+        return answer + bridge_length;
+/*      Queue<truck> weightsQueue = new LinkedList<>();
         Queue<truck> bridgeLengthQueue = new LinkedList<>();
         for (int truck_weight : truck_weights) {
             truck truck = new truck(truck_weight);
             weightsQueue.add(truck);
         }
 
-        int answer = 0;
+       int answer = 0;
         int total_weight = 0;
 
         while( !bridgeLengthQueue.isEmpty() || !weightsQueue.isEmpty() ){
@@ -45,16 +66,16 @@ public class 다리를_지나는_트럭 {
 
         }
 
-        /**
+        *//**
          * 1. 트럭을 보낸다
          * 2. 다리 길이와 견딜 수 잇는 무게를 고려한다.
          * 3. 최소 다리의 길이만큼 시간이 지나야 다리를 건넌것이다.
-         */
+         *//*
 
-        return answer+1;
+        return answer+1;*/
     }
 
-    class truck {
+/*    class truck {
         int weight;
         int location ;
 
@@ -62,7 +83,7 @@ public class 다리를_지나는_트럭 {
             this.weight = weight;
             this.location = 1;
         }
-    }
+    }*/
 
 
     public static void main(String[] args) {
