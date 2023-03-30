ec517320bb453a701857b6efdf65ce4256c221e6
0320kangk <0320kangk@naver.com>
2023-03-30 23:46:35 +0900

    더 맵게 플이

--- src/heap/더_맵게/더_맵게.java
+++ src/heap/더_맵게/더_맵게.java
@@ -1,31 +1,26 @@
-package heap;
+package heap.더_맵게;
 
 import java.util.PriorityQueue;
 
 public class 더_맵게 {
-
-
     public int solution(int[] scoville, int K) {
-
         int answer = 0;
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i : scoville) {
-            priorityQueue.add(i);
+            pq.add(i);
         }
-        while ( priorityQueue.size() >1 && priorityQueue.peek() < K ) {
 
-            Integer firstSmall = priorityQueue.poll();
-            Integer secondSmall = priorityQueue.poll();
-            priorityQueue.add(firstSmall + secondSmall * 2);
+        while ( pq.size() > 1 && pq.peek() < K) {
+            Integer food1 = pq.poll();
+            Integer food2 = pq.poll();
+            pq.add(food1 + food2 * 2);
             answer++;
-
         }
-        if (priorityQueue.peek() < K) {
+
+        if (pq.peek() < K) {
             return -1;
         }
-
-
         return answer;
     }
 
@@ -34,7 +29,4 @@ public class 더_맵게 {
         int solution = 더_맵게.solution(new int[]{1, 2, 3, 9, 10, 12}, 7);
         System.out.println(solution);
     }
-
-
-
 }
