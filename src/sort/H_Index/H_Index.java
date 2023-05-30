627610f6555d735f15f5bd4f8d734436c4924f4b
0320kangk <0320kangk@naver.com>
2023-05-30 21:12:07 +0900

    전력망을 둘로 나누기 풀이 및 코드

--- src/sort/H_Index/H_Index.java
+++ src/sort/H_Index/H_Index.java
@@ -1,33 +1,24 @@
-package sort;
-
-import com.sun.tools.attach.AttachNotSupportedException;
+package sort.H_Index;
 
 public class H_Index {
 
     public int solution(int[] citations) {
 
-        int answer = 0;
-
-        int length = citations.length;
-        int h = length;
-
-        // [1,1,1,2,2]
-        while (h > 0) {
-            //인용된 논문 수
-            int quotationCount = 0;
+        int h = citations.length;
+        int count = 0;
+        //논문 h번 이상 논문 h번 개수 이상 만족, 나머지 h 이하 인용
+        while ( true ) {
+            count=0;
             for (int citation : citations) {
                 if (citation >= h) {
-                    quotationCount++;
+                    count++;
                 }
             }
-            if ( quotationCount >= h  && length - quotationCount <= h ) {
-                answer = h;
-                break;
+            if (count >= h) {
+                return h;
             }
             h--;
         }
-
-        return answer;
     }
 
     public static void main(String[] args) {
