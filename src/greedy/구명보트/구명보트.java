ae7c8e5ed2bf68c31285ff75d82a064f933a9da2
0320kangk <0320kangk@naver.com>
2023-06-18 16:34:24 +0900

    구명 보트 코드 수정

--- src/greedy/구명보트/구명보트.java
+++ src/greedy/구명보트/구명보트.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class 구명보트 {
 
     public int solution(int[] people, int limit) {
-        int answer = people.length;
+        int answer = 0;
         Arrays.sort(people);
         int min = 0;
         for (int max = people.length - 1; min <= max; max--){
--- src/greedy/구명보트/구명보트2.java
+++ src/greedy/구명보트/구명보트2.java
@@ -1,2 +1,22 @@
-package greedy.구명보트;public class 구명보트2 {
+package greedy.구명보트;
+
+import java.util.Arrays;
+
+public class 구명보트2 {
+    public int solution(int[] people, int limit) {
+
+        int answer = 0;
+
+        Arrays.sort(people);
+
+        int min = 0;
+        for (int max = people.length - 1; min <= max; max--) {
+            if ( people[max] + people[min] <= limit)
+                min++;
+            answer++;
+        }
+        return answer;
+
+    }
+
 }
